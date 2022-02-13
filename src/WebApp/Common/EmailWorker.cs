26f49c214b3b13478177d1990a96f034c99cac18
Hakim Ghods <ghods.hakim@hotmail.com>
2022-02-13 11:56:13 +0330

    safe

--- src/WebApp/Common/EmailWorker.cs
+++ src/WebApp/Common/EmailWorker.cs
@@ -17,24 +17,27 @@
                 if (_taskJob.Queue.Any())
                 {
                     _logger.LogDebug("{0} message queued.", _taskJob.Queue.Count);
-                    var taskList=new List<Task>();
+                    var taskList = new List<Task>();
 
-                    var queueCount=_taskJob.Queue.Count;
+                    var queueCount = _taskJob.Queue.Count;
 
-                    for (int i=0; i<queueCount; i++)
+                    for (int i = 0; i < queueCount; i++)
                     {
-                        var msg = _taskJob.Queue.Dequeue();
-                        var t = SendEmail(msg);
-                        taskList.Add(t);
+                        var success = _taskJob.Queue.TryDequeue(out var msg);
+                        if (success)
+                        {
+                            var t = SendEmail(msg);
+                            taskList.Add(t);
+                        }
                     }
 
-                    
+
                     await Task.WhenAll(taskList);
                     _logger.LogDebug("{0} emails sent.", queueCount);
 
 
                 }
-                
+
                 await Task.Delay(1000, stoppingToken);
                 //_logger.LogDebug("Backgroud service executed.");
             }
