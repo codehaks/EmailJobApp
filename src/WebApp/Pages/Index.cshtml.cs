5a5b2c2929037cb2e8af37a533b5b5c6cde5560b
Hakim Ghods <ghods.hakim@hotmail.com>
2022-04-10 12:30:05 +0430

    client

--- src/WebApp/Pages/Index.cshtml.cs
+++ src/WebApp/Pages/Index.cshtml.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RabbitMQ.Client;
 
 namespace WebApp.Pages;
+
+class Email
+{
+    public string Message { get; set; }
+}
 
 public class IndexModel : PageModel
 {
@@ -19,7 +25,21 @@ public class IndexModel : PageModel
 
     public IActionResult OnPost()
     {
-        _taskJob.Queue.Enqueue(Message);
+        //_taskJob.Queue.Enqueue(Message);
+
+        var factory = new ConnectionFactory();
+        var connection=factory.CreateConnection();
+        var channel = connection.CreateModel();
+
+        channel.ExchangeDeclare("email-box", ExchangeType.Direct);
+        channel.QueueDeclare("email-send-list", false, false, false, null);
+        channel.QueueBind("email-send-list", "email-box", "email-add");        
+
+        var email=new Email { Message=Message};
+        var json = System.Text.Json.JsonSerializer.Serialize(email);
+        var bytes=System.Text.Encoding.UTF8.GetBytes(json);
+        channel.BasicPublish("email-box", "email-add",null,bytes);
+
         TempData["status"] = "Email sent.";
         _logger.LogDebug("Email sent by user.");
         return Page();
